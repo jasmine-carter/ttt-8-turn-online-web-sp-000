0813b95c8bd42b27c1b97c80de9863cf6d02c7c1
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-06 00:30:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input.to_i - 1
+  index = user_input.to_i - 1
 end
 
 def valid_move?(board, index)
@@ -28,7 +28,7 @@ def turn(board)
 puts "Please enter 1-9:"
 user_input = gets.chomp
 input_to_index(user_input)
-until user_input.between?(0,9)
+until .between?(0,9)
   turn
 end
 end
